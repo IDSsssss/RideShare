85a3fe789255498b05002d32413092fce5dfd6c1
ivan <i_shevchenko@tut.by>
2026-03-19 09:45:06 +0300

    EntityGraph; n+1

--- src/main/java/com/example/rideshare/model/entity/Booking.java
+++ src/main/java/com/example/rideshare/model/entity/Booking.java
@@ -1,7 +1,18 @@
 package com.example.rideshare.model.entity;
 
 import com.example.rideshare.model.enums.BookingStatus;
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.FetchType;
 import lombok.Data;
 import java.time.LocalDateTime;
 
