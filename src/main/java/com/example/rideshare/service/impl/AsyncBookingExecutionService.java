c3644fb669f91f62d3f9dc1a5d064eea8a697e39
ivan <i_shevchenko@tut.by>
2026-04-28 09:23:50 +0300

    lab06

--- src/main/java/com/example/rideshare/service/impl/AsyncBookingExecutionService.java
+++ src/main/java/com/example/rideshare/service/impl/AsyncBookingExecutionService.java
@@ -22,7 +22,7 @@ public class AsyncBookingExecutionService {
         taskTracker.updateStatus(taskId, "PROCESSING");
 
         try {
-            Thread.sleep(3000);
+            Thread.sleep(10000);
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
             taskTracker.failTask(taskId, "Task was interrupted");
