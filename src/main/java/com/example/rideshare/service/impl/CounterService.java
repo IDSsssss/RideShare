d842852d3ae5d10afbba60cb8b7f0cee8f31d484
ivan <i_shevchenko@tut.by>
2026-04-28 13:55:09 +0300

    lab06

--- src/main/java/com/example/rideshare/service/impl/CounterService.java
+++ src/main/java/com/example/rideshare/service/impl/CounterService.java
@@ -8,7 +8,6 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.atomic.AtomicLong;
 
 @Slf4j
 @Service
