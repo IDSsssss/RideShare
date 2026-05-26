6d429bf50fdbaaad33f0524f6af937e27e9964b3
ivan <i_shevchenko@tut.by>
2026-05-26 21:32:12 +0300

    lab07

--- src/test/java/com/example/rideshare/service/impl/ReviewServiceImplTest.java
+++ src/test/java/com/example/rideshare/service/impl/ReviewServiceImplTest.java
@@ -18,6 +18,7 @@ import com.example.rideshare.repository.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
